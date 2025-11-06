3388bf3a4b77078fe5290a086a905f9ff4a4283b
e.eremin <e.eremin@leningrad.io>
2025-11-06 15:25:44 +0300

    __

--- shared_packages/lib/loadEnv.ts
+++ shared_packages/lib/loadEnv.ts
@@ -1,8 +1,11 @@
-// shared_packages/lib/actions/env.ts
+// shared_packages/lib/loadEnv.ts
 import path from "path";
 import dotenv from "dotenv";
 
-dotenv.config({ path: path.resolve(__dirname, "../../..", ".env") });
+// process.cwd() points to where Node was started (subproject root)
+if (!process.env.HAQ_BOT_NAME) {
+  dotenv.config({ path: path.resolve(process.cwd(), "../../.env") });
+}
 
 export const HAQ_BOT_NAME = process.env.HAQ_BOT_NAME;
 export const HAQ_BOT_PASS = process.env.HAQ_BOT_PASS;
